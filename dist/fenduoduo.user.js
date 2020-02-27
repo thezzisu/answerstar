2d049e0eea34200665b42a0b2a1ac54025144690
ZhangZisu <admin@zhangzisu.cn>
2020-02-27 17:23:58 +0800

    Rebuild

--- dist/fenduoduo.user.js
+++ dist/fenduoduo.user.js
@@ -57,98 +57,136 @@
 }([ function(module, exports, __webpack_require__) {
     /ks\.wjx\.top/.test(window.location.href) && __webpack_require__(1);
 }, function(module, exports) {
-    console.log("WJX Detected"), /(ks\.wjx\.top\/m\/)/.test(location.href) && (location.href = location.href.replace(/m/, "jq"));
-    const tid = /([0-9]+)\.aspx$/.exec(location.href)[1];
-    window.addEventListener("load", () => {
-        setTimeout(() => {
-            document.oncontextmenu = null, document.ondragstart = null, document.onselectstart = null, 
-            document.querySelectorAll(".fieldset").forEach(fs => {
-                fs.style.display = "";
-            }), document.getElementById("submit_table").style.display = "", document.getElementById("btnNext").parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none", 
-            console.log("FenDuoDuo is loaded");
-            const problems = [ ...document.querySelectorAll(".div_question").values() ].map(x => function(elem) {
+    console.log("WJX Detected");
+    let tid, problems = [];
+    function allowCopyPaste() {
+        document.oncontextmenu = null, document.ondragstart = null, document.onselectstart = null;
+    }
+    function parseKsPage() {
+        const divs = document.querySelectorAll(".div_question");
+        problems = [ ...divs.values() ].map(x => function(elem) {
+            let result;
+            if (result = function(elem) {
                 const c = elem.querySelector(".div_table_radio_question"), id = elem.id.substr(3);
-                return c.querySelector("a.jqCheckbox") || c.querySelector("a.jqRadio") ? {
-                    type: "c",
-                    elem: elem,
-                    id: id
-                } : {
-                    type: "unknow",
-                    elem: elem,
-                    id: id
-                };
-            }(x));
-            function parseC(elem) {
-                const input = elem.querySelector("input");
-                return /^(.+)_/.exec(input.id)[1];
-            }
-            function get(elem, type) {
-                switch (type) {
-                  case "c":
-                    return function(elem) {
-                        const checked = [ ...elem.querySelectorAll("a.jqChecked").values() ];
-                        if (checked.length) {
-                            const b = parseC(elem);
-                            return checked.map(x => x.rel.substr(b.length + 1)).join(",");
+                if (c.querySelector("a.jqCheckbox") || c.querySelector("a.jqRadio")) {
+                    const cid = _utilsParseCID(elem), o = [ ...elem.querySelectorAll("label").values() ].filter(x => x.htmlFor.startsWith(cid)).map(x => [ x.htmlFor.substr(cid.length + 1), x.textContent.trim() ]), t = c.querySelector("a.jqCheckbox") ? 1 : 0;
+                    return {
+                        type: "c",
+                        elem: elem,
+                        id: id,
+                        meta: {
+                            o: o,
+                            t: t
                         }
-                        return "";
-                    }(elem);
+                    };
                 }
-                return console.group("unknow element"), console.log(`Type ${type}`), console.log(elem), 
-                console.groupEnd(), "";
+            }(elem)) return result;
+            console.group("Unknow problem"), console.log(elem), console.groupEnd();
+        }(x)).filter(x => x);
+        const problemsMeta = problems.map(x => ({
+            id: x.id,
+            type: x.type,
+            meta: x.meta
+        }));
+        localStorage.setItem(tid + ".p", JSON.stringify(problemsMeta)), console.log(problems);
+    }
+    function _utilsParseCID(elem) {
+        const input = elem.querySelector("input");
+        return /^(.+)_/.exec(input.id)[1];
+    }
+    function get(elem, type) {
+        switch (type) {
+          case "c":
+            return function(elem) {
+                const checked = [ ...elem.querySelectorAll("a.jqChecked").values() ];
+                if (checked.length) {
+                    const b = _utilsParseCID(elem);
+                    return checked.map(x => x.rel.substr(b.length + 1)).join(",");
+                }
+                return "";
+            }(elem);
+        }
+        return "";
+    }
+    function set(elem, type, val, override) {
+        if (override || !get(elem, type)) switch (type) {
+          case "c":
+            return function(elem, result) {
+                if (!result) return;
+                const b = _utilsParseCID(elem), options = result.split(",");
+                for (const o of options) {
+                    const lab = elem.querySelector(`a[rel="${b}_${o}"]`);
+                    lab && lab.click();
+                }
+            }(elem, val);
+        }
+    }
+    function ksGetAll() {
+        const m = localStorage.getItem(tid), map = m ? JSON.parse(m) : Object.create(null);
+        for (const p of problems) {
+            const v = get(p.elem, p.type);
+            v && (map[p.id] = v);
+        }
+        const v = JSON.stringify(map);
+        return localStorage.setItem(tid, v), v;
+    }
+    function generateLink(val) {
+        return [ tid, btoa(val) ].join("$");
+    }
+    function getData() {
+        return generateLink(ksGetAll());
+    }
+    function initUI() {
+        const container = document.createElement("div");
+        function hideMenu() {
+            container.style.display = "none";
+        }
+        function createBtn(text, cb) {
+            const b = document.createElement("button");
+            b.textContent = text, b.addEventListener("click", cb), container.appendChild(b);
+        }
+        return container.style.zIndex = 999, container.style.position = "fixed", container.style.top = "32px", 
+        container.style.left = "32px", document.body.appendChild(container), hideMenu(), 
+        createBtn("X", () => {
+            hideMenu();
+        }), function(cb) {
+            const a = document.createElement("button");
+            a.textContent = "menu", a.style.zIndex = 998, a.style.position = "fixed", a.style.bottom = "32px", 
+            a.style.right = "32px", document.body.appendChild(a), a.addEventListener("click", cb);
+        }(() => {
+            container.style.display = "";
+        }), console.log("UI Init"), createBtn;
+    }
+    function jgParseResult() {
+        return [ ...document.querySelectorAll('img[alt="错误"]').values() ].map(x => function(elem) {
+            const top = elem.parentElement.parentElement.parentElement, id = top.getAttribute("topic"), p = problems.find(x => x.id === id);
+            if ("c" === p.type && 0 === p.meta.t) {
+                const val = top.querySelector("div.data__key > div > font").nextSibling.textContent.trim(), right = p.meta.o.find(x => x[1] === val)[0];
+                return [ id, right ];
             }
-            function set(elem, type, val, override) {
-                if (override || !get(elem, type)) {
-                    switch (type) {
-                      case "c":
-                        return function(elem, result) {
-                            if (!result) return;
-                            const b = parseC(elem), options = result.split(",");
-                            for (const o of options) {
-                                const lab = elem.querySelector(`a[rel="${b}_${o}"]`);
-                                lab && lab.click();
-                            }
-                        }(elem, val);
-                    }
-                    console.group("unknow element"), console.log(`Type ${type}`), console.log(elem), 
-                    console.groupEnd();
-                }
-            }
-            function getAll() {
-                const m = localStorage.getItem(tid), map = m ? JSON.parse(m) : Object.create(null);
-                for (const p of problems) {
-                    const v = get(p.elem, p.type);
-                    v && (map[p.id] = v);
-                }
-                const v = JSON.stringify(map);
-                return localStorage.setItem(tid, v), v;
-            }
-            function getData() {
-                return [ tid, btoa(getAll()) ].join("$");
-            }
-            !function() {
-                const container = document.createElement("div");
-                function hideMenu() {
-                    container.style.display = "none";
-                }
-                function createBtn(text, cb) {
-                    const b = document.createElement("button");
-                    b.textContent = text, b.addEventListener("click", cb), container.appendChild(b);
-                }
-                container.style.zIndex = 999, container.style.position = "fixed", container.style.top = "32px", 
-                container.style.left = "32px", document.body.appendChild(container), hideMenu(), 
-                createBtn("X", () => {
-                    hideMenu();
-                }), createBtn("Export", () => {
+        }(x)).filter(x => x);
+    }
+    switch (/ks\.wjx\.top\/jq\//.test(location.href) ? 1 : /ks\.wjx\.top\/wjx\/join\//.test(location.href) ? 2 : void 0) {
+      case 1:
+        window.addEventListener("load", () => {
+            setTimeout(() => {
+                allowCopyPaste(), function() {
+                    const match = /([0-9]+)\.aspx$/.exec(location.href);
+                    tid = match[1];
+                }(), document.querySelectorAll(".fieldset").forEach(fs => {
+                    fs.style.display = "";
+                }), document.getElementById("submit_table").style.display = "", document.getElementById("btnNext").parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none", 
+                parseKsPage();
+                const btn = initUI();
+                btn("Export", () => {
                     const s = getData();
                     prompt("Your answer:", s);
-                }), createBtn("Import", () => {
+                }), btn("Import", () => {
                     !function(val) {
                         const [ttid, pld] = val.split("$");
-                        if (ttid !== tid) return void alert("Not for this paper");
-                        localStorage.setItem(tid, atob(pld));
+                        ttid === tid ? localStorage.setItem(tid, atob(pld)) : alert("Not for this paper");
                     }(prompt("Please paste"));
-                }), createBtn("Apply", () => {
+                }), btn("Apply", () => {
                     !function() {
                         const map = JSON.parse(localStorage.getItem(tid));
                         for (const id in map) {
@@ -156,24 +194,32 @@
                             p ? set(p.elem, p.type, map[id], !1) : console.warn(`ID ${id} not found`);
                         }
                     }();
-                });
-                {
-                    const a = document.createElement("button");
-                    a.textContent = "menu", a.style.zIndex = 998, a.style.position = "fixed", a.style.bottom = "32px", 
-                    a.style.right = "32px", document.body.appendChild(a), a.addEventListener("click", () => {
-                        container.style.display = "";
+                }), function() {
+                    const submitBtn = document.getElementById("submit_button"), bk = submitBtn.onclick;
+                    submitBtn.onclick = null, submitBtn.addEventListener("click", ev => {
+                        const s = getData();
+                        return prompt("Your answer:", s), confirm("Are you sure to submit?") ? bk(ev) : (ev.preventDefault(), 
+                        !1);
+                    }), window.addEventListener("click", () => {
+                        ksGetAll();
                     });
-                }
-                console.log("UI Init");
-            }();
-            const submitBtn = document.getElementById("submit_button"), bk = submitBtn.onclick;
-            submitBtn.onclick = null, submitBtn.addEventListener("click", ev => {
-                const s = getData();
-                return prompt("Your answer:", s), confirm("Are you sure to submit?") ? bk(ev) : (ev.preventDefault(), 
-                !1);
-            }), window.addEventListener("click", () => {
-                getAll();
-            });
-        }, 200);
-    });
+                }();
+            }, 200);
+        }), /(ks\.wjx\.top\/m\/)/.test(location.href) && (location.href = location.href.replace(/m/, "jq"));
+        break;
+
+      case 2:
+        window.addEventListener("load", () => {
+            setTimeout(() => {
+                allowCopyPaste(), function() {
+                    const match = /q=([0-9]+)/.exec(location.search);
+                    tid = match[1];
+                }(), problems = JSON.parse(localStorage.getItem(tid + ".p")), initUI();
+                const delta = jgParseResult(), map = JSON.parse(localStorage.getItem(tid));
+                for (const d of delta) map[d[0]] = d[1];
+                const value = JSON.stringify(map);
+                localStorage.setItem(tid + ".r", value), prompt("Right answer:", generateLink(value));
+            }, 200);
+        });
+    }
 } ]);
